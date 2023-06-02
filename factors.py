dc7981c63345e481661cfb18cbb30e65350dd9a9
Reem99-sina <reemsina2@gmail.com>
2023-06-02 04:27:00 -0700

    factors.py

--- factors.py
+++ factors.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 import sys
-
+import signal
+def handler(signum, frame):
+    print("Time's up!")
+    raise SystemExit
 def factorize(n):
     """
     Factorize a number into a product of two smaller numbers.
     Returns a tuple (p, q), where p and q are the factors.
     If no factorization is possible, returns None.
     """
+    signal.alarm(1)
+    signal.signal(signal.SIGALRM, handler)
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return (i, n // i)
+    signal.alarm(0)
     return None
+
 
 def factor_file(filename):
     """
     Factorize each number in a file into a product of two smaller numbers.
     Outputs the factorization for each number in the format n=p*q.
     """
+    
     with open(filename, "r") as f:
         for line in f:
             n = int(line.strip())
+            
             factors = factorize(n)
             if factors is not None:
                 p, q = factors
